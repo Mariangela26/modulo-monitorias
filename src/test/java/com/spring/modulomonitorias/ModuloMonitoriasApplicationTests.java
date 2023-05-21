f480f0d71cda46a47af82e2c6ff59b1b074176f4
mariano <mariano@bancolombia.com.co>
2023-05-21 17:27:47 -0500

    commit inicial

---
+++ src/test/java/com/spring/modulomonitorias/ModuloMonitoriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.modulomonitorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloMonitoriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
